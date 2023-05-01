250912f4acb24cb29f2613cb5b41e6729285f2f3
pitamchandra <pitamchandra402@gmail.com>
2023-05-01 22:42:32 +0600

    install react router dom, firebase, and done basic router setup

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Main from './Layout/Main.jsx'
+import Error from './components/Error/Error.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Main></Main>,
+    errorElement : <Error></Error>,
+    children : [
+      {
+        path : '/',
+        element : <Home></Home>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
